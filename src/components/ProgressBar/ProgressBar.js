b769b063cfc3987b64b1e576a7dd9e4bfb9e8c06
Nicolas Salhuana <nicolas.salhuana@gmail.com>
2023-04-25 15:28:40 -0700

    finished progress bar with progress tag

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,79 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const SIZES = {
+  small: {
+    '--height': 8 + 'px',
+    '--borderRadius': 4 + 'px',
+    '--innerBorderRadius': 4 + 'px',
+  },
+  medium: {
+    '--height': 12 + 'px',
+    '--borderRadius': 4 + 'px',
+    '--innerBorderRadius': 4 + 'px',
+  },
+  large: {
+    '--height': 24 + 'px',
+    '--borderRadius': 8 + 'px',
+    '--innerBorderRadius': 4 + 'px',
+    '--padding': 4 + 'px',
+  },
+};
+
+// We need to figure out how to make small numbers fit inside
+// We could use a padding-left so that it always looks at least 10% done
+// Round as necessary
+const Progress = styled.progress`
+  height: var(--height);
+
+  // Reset appearance
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+
+  /* Get rid of default border in Firefox. */
+  border: none;
+  // End reset appearance
+
+  &::-webkit-progress-bar {
+    background-color: ${COLORS.transparentGray15};
+    box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+    border-radius: var(--borderRadius);
+    padding: var(--padding, 0);
+  }
+
+  &::-webkit-progress-value {
+    background-color: ${COLORS.primary};
+    border-radius: var(--innerBorderRadius) 0 0 var(--innerBorderRadius);
+    padding-left: 16px;
+
+    // Round as necessary
+    ${(p) =>
+      p.value > 98 &&
+      css`
+        border-radius: var(--innerBorderRadius);
+      `}
+  }
+`;
+
+const ProgressBar = ({ value, size, label }) => {
+  if (!value) {
+    value = 0;
+  }
+  const styles = SIZES[size];
+
+  console.log(styles);
+
+  return (
+    <>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <Progress max="100" value={value} style={styles} />
+    </>
+  );
 };
 
 export default ProgressBar;
