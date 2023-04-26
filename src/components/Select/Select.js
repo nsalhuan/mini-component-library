163aad57a89430321950990b90889328c9efa027
Nicolas Salhuana <nicolas.salhuana@gmail.com>
2023-04-25 18:30:21 -0700

    watched the video

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -6,12 +6,46 @@ import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 import VisuallyHidden from '../VisuallyHidden/VisuallyHidden';
 
-const DummySelectDiv = styled.div`
+const Wrapper = styled.div`
+  width: max-content;
   position: relative;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  width: fit-content;
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+
+  right: 10px;
+
+  // Center vertically
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  width: var(--size);
+  height: var(--size);
+  pointer-events: none;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  opacity: 0;
+
+  // Remove dropdown arrow
+  /* -o-appearance: none;
+  -ms-appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none; */
+  appearance: none;
+
+  width: 100%;
+  height: 100%;
+
+  border-radius: 8px;
+`;
+
+const PresentationalBit = styled.div`
   border-radius: 8px;
   padding: 12px 52px 12px 16px;
   background-color: ${COLORS.transparentGray15};
@@ -20,49 +54,14 @@ const DummySelectDiv = styled.div`
   font-weight: 400;
   line-height: 1.17;
 
-  &:hover {
-    color: black;
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
   }
-`;
 
-const Wrapper = styled.div`
-  width: fit-content;
-  position: relative;
-`;
-
-const IconWrapper = styled.div`
-  display: inline-block;
-  position: absolute;
-
-  right: 10px;
-
-  // Center vertically
-  top: 50%;
-  transform: translateY(-50%);
-`;
-
-const HiddenSelect = styled.select`
-  position: absolute;
-  top: 0;
-  left: 0;
-
-  color: transparent;
-  background-color: transparent;
-  opacity: 1;
-  z-index: 1;
-
-  // Remove dropdown arrow
-  -o-appearance: none;
-  -ms-appearance: none;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-
-  width: 100%;
-  height: 100%;
-
-  border: none;
-  border-radius: 8px;
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
 `;
 
 const Select = ({ label, value, onChange, children }) => {
@@ -73,15 +72,15 @@ const Select = ({ label, value, onChange, children }) => {
   return (
     <Wrapper>
       <VisuallyHidden>{label}</VisuallyHidden>
-      <DummySelectDiv>
-        {displayedValue}
-        <IconWrapper>
-          <Icon id="chevron-down" strokeWidth="2" />
-        </IconWrapper>
-      </DummySelectDiv>
-      <HiddenSelect value={value} onChange={onChange} style={{}}>
+      <NativeSelect value={value} onChange={onChange} style={{}}>
         {children}
-      </HiddenSelect>
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <IconWrapper style={{ '--size': 24 + 'px' }}>
+          <Icon id="chevron-down" strokeWidth="2" size={24} />
+        </IconWrapper>
+      </PresentationalBit>
     </Wrapper>
   );
 };
